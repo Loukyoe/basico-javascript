34fa692feb76f075535670f6698099e4f75359f5
Gabriela <iwantsmilelove@gmail.com>
2024-06-01 22:09:25 -0300

    Adiciona atividades da aula 03

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,13 +1,21 @@
 console.log("Trabalhando com atribuição de variáveis");
-const idade = 19; 
-let nome = "Gabs";
+ 
+let primeiroNome = "Gabs";
 const sobrenome = "Flores";
 
 //console.log(nome, sobrenome);
-console.log(nome + " " + sobrenome);
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(primeiroNome + " " + sobrenome);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador +1;
 
 const nomeCompleto = nome + sobrenome; //sobrescrita de variável
 console.log(nomeCompleto);
 
 // variável que não varia :p nomeCompleto = 2;
+
+let idade; 
+idade = 26;
+idade = idade+1;
+console.log(idade);
